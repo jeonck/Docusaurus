ccea00db89ba1cbdb2bb8e6c0f8b5df729c5ec78
전창국 Digital Twin Platform개발팀 <ck.jeon@sk.com>
2025-04-29 12:53:42 +0900

    fix: baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://jeonck.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Docusaurus',
+  baseUrl: '/Docusaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
